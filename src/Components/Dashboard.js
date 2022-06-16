86dfd5734ac3fc386450013c041941d5a2add20c
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-06-16 12:33:25 +0600

    make responsive

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -7,9 +7,17 @@ const Dashboard = () => {
   return (
     <div>
       <div className=" h-[7vh] w-full bg-slate-600 flex items-center justify-between">
-        <h1 className=" flex items-center justify-center h-[7vh] w-[20vw] text-3xl font-bold text-white bg-red-200">
+        <h1 className=" flex items-center justify-center h-[7vh] w-[20vw] text-3xl font-bold text-white">
           Logo
         </h1>
+        <div className=" md:hidden lg:hidden flex gap-2">
+          <p>
+            <Link to="/dashboard/charts">Charts</Link>
+          </p>
+          <p>
+            <Link to="/dashboard/list">Employee List</Link>
+          </p>
+        </div>
         <button
           onClick={() => signOut(auth)}
           className=" btn btn-ghost mr-5 text-white"
@@ -18,7 +26,7 @@ const Dashboard = () => {
         </button>
       </div>
       <div className=" flex">
-        <div className=" h-[93vh] w-[20vw] bg-slate-600 py-10">
+        <div className=" h-[93vh] w-[20vw] bg-slate-600 py-10 hidden md:block lg:block">
           <p>
             <Link to="/dashboard/charts">Charts</Link>
           </p>
