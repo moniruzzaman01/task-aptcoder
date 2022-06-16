bd01abcac0e874a3d577b120f4ad34bf6a7e7091
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-06-16 11:39:21 +0600

    login done

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SocialLogin from "./SocialLogin";
 
 const Login = () => {
   return (
@@ -9,21 +10,18 @@ const Login = () => {
           type="email"
           name="email"
           placeholder="Email"
-          class="input input-bordered input-primary w-full max-w-lg mb-5"
+          className="input input-bordered input-primary w-full max-w-lg mb-5"
         />
         <input
           type="password"
           name="pass"
           placeholder="Password"
-          class="input input-bordered input-primary w-full max-w-lg mb-2"
+          className="input input-bordered input-primary w-full max-w-lg mb-2"
         />
         <p className=" text-left mb-5">New Here? Create an account.</p>
         <button className=" btn btn-primary px-10">Login</button>
       </form>
-      <div class="flex flex-col w-full border-opacity-50">
-        <div class="divider">OR</div>
-      </div>
-      <button className=" btn btn-secondary px-10">Google Login</button>
+      <SocialLogin />
     </div>
   );
 };
---
+++ src/Components/SocialLogin.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../firebase.init";
+
+const SocialLogin = () => {
+  const [googleSignIn] = useSignInWithGoogle(auth);
+  const handleGoogleLogin = () => {
+    googleSignIn();
+  };
+  return (
+    <div>
+      <div className="flex flex-col w-full border-opacity-50">
+        <div className="divider">OR</div>
+      </div>
+      <button onClick={handleGoogleLogin} className=" btn btn-secondary px-10">
+        Google Login
+      </button>
+    </div>
+  );
+};
+
+export default SocialLogin;
